161894bd5295dfee754d86732f5d24c4c80a9eeb
jamiaditya <jamiaditya@gmail.com>
2018-06-11 14:58:12 +0530

    Performance changes

--- DataStructuresJavaprograms/BinaryFibonacciSumPossibilities.java
+++ DataStructuresJavaprograms/BinaryFibonacciSumPossibilities.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class BinaryFibonacciSumPossibilities {
@@ -7,15 +8,17 @@ public class BinaryFibonacciSumPossibilities {
 	public static long count = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 150;/////////////////////////Enter the number here
-		HashMap<Integer, Integer> newHash = new HashMap<>();
-		ArrayList<Integer> array = new ArrayList<>(sum/2);
-		for (int i=0; i<=sum; i++) {
+		System.out.println(new Date());
+		long sum = 200;/////////////////////////Enter the number here
+		long n = sum>=100?sum/5:sum;
+		HashMap<Long, Long> newHash = new HashMap<>();
+		ArrayList<Long> array = new ArrayList<>();
+		for (long i=0; i<=sum; i++) {
 			newHash.put(i, null);
 		}
-		getFibSeriesArray(sum, newHash, sum);
-		for (int i=1;i<newHash.keySet().size(); i++) {
-			if(newHash.get(i)!=null && newHash.get(i)>=0)array.add(i-1, newHash.get(i));
+		getFibSeriesArray(n, newHash, sum);
+		for (long i=1;i<newHash.keySet().size(); i++) {
+			if(newHash.get(i)!=null && newHash.get(i)>=0)array.add((int) (i-1), newHash.get(i));
 			else {
 				break;
 			}
@@ -23,23 +26,24 @@ public class BinaryFibonacciSumPossibilities {
 		System.out.println(array);
 		HashMap<String, String> combinationHash = new HashMap<>();
 		String combination = "";
-		for(int i=array.size()-1;i>=0;i--) {
+		for(long i=array.size()-1;i>=0;i--) {
 			getSum(array, i, sum, combination, combinationHash);
 			combination = combination+"0";
 		}
 		System.out.println(combinationHash.keySet());
+		System.out.println(new Date());
 	}
 	
-	public static void getSum(ArrayList<Integer> array, int i, int sum, String combination, HashMap<String, String> hash) {
-		for(int index=i;index>=0; index--) {
-			int valueAtIndex = array.get(index);
+	public static void getSum(ArrayList<Long> array, long i, long sum, String combination, HashMap<String, String> hash) {
+		for(long index=i;index>=0; index--) {
+			long valueAtIndex = array.get((int) index);
 			sum = sum - valueAtIndex;
 			if(sum==0) {
 				String c = combination;
 				if(c.length()==0)c = valueAtIndex+"";
 				else c = valueAtIndex + SEPERATOR+c;
-				int prefix = array.size() - c.split(SEPERATOR).length;
-				for(int j = 0;j<prefix;j++) {
+				long prefix = array.size() - c.split(SEPERATOR).length;
+				for(long j = 0;j<prefix;j++) {
 					c="0"+SEPERATOR+c;
 				}
 				hash.put(c, c);
@@ -61,12 +65,13 @@ public class BinaryFibonacciSumPossibilities {
 		}
 	}
 	
-	public static int getFibSeriesArray(int n, HashMap<Integer, Integer> hash, int x) {
+	public static long getFibSeriesArray(long n, HashMap<Long, Long> hash, long x) {
+		//System.out.println(count++);
 		try {
 			if(hash.get(n)==null) {
 				if(n<=1)hash.put(n, n);
 				else if(n>1) {
-					int number = getFibSeriesArray(n-1, hash, x) + getFibSeriesArray(n-2, hash, x);
+					long number = getFibSeriesArray(n-1, hash, x) + getFibSeriesArray(n-2, hash, x);
 					if(x>=number)hash.put(n, number);
 				}
 			}
